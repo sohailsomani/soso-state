61d5a71aa9ed93179231b491a7b35820195ef2ec
Sohail Somani <me@sohailsomani.com>
2021-01-24 14:47:29 -0500

    Add comments

--- tests/test_README.py
+++ tests/test_README.py
@@ -28,12 +28,15 @@ class AppModel(state.Model[AppState]):
 class TestREADME(unittest.TestCase):
     def test_README(self) -> None:
         app = AppModel()
+        # Add some initial values
         app.update(
             regional_managers=[Person("Michael", "Scott")],
             assistant_to_the_regional_managers=[Person("Dwight", "Schrute")],
             employees=[Person("Jim", "Halpert"),
                        Person("Pam", "Beesly")])
 
+        # Ensure that the values actually were persisted to the application
+        # state
         self.assertEqual(app.state.regional_managers,
                          [Person("Michael", "Scott")])
         self.assertEqual(app.state.assistant_to_the_regional_managers,
@@ -43,14 +46,22 @@ class TestREADME(unittest.TestCase):
                           Person("Pam", "Beesly")])
 
         x: AppState
-        mock = MagicMock()
-        token = app.subscribe(lambda x: x.regional_managers[0], mock)
-        mock.assert_called_with(Person("Michael", "Scott"))
+        regional_manager = MagicMock()
+        # Subscribe to changes in the first regional manager, a token is
+        # returned to allow you to disconnect later if needed.
+        token = app.subscribe(lambda x: x.regional_managers[0],
+                              regional_manager)
+        # Whenever we subscribe, the callback is always initially called with
+        # the current value.
+        regional_manager.assert_called_with(Person("Michael", "Scott"))
 
         pams_last_name = MagicMock()
         app.subscribe(lambda x: x.employees[1].last_name, pams_last_name)
         pams_last_name.assert_called_with("Beesly")
 
+        # For more complex property changes, use a function. Note that the
+        # "state" passed in is a proxy object that only records what values you
+        # set so do not attempt to read from it.
         def pam_gets_married(state: AppState) -> None:
             state.employees[1].last_name = "Halpert"
 
@@ -58,14 +69,18 @@ class TestREADME(unittest.TestCase):
         app.update(pam_gets_married)
 
         self.assertEqual(app.state.employees[1].last_name, "Halpert")
+        # Note that the callback was called with exactly the attribute that was
+        # subscribed to: x.employees[1].last_name
         pams_last_name.assert_called_with("Halpert")
 
-        mock.reset_mock()
+        regional_manager.reset_mock()
         app.update(regional_managers=[Person("Dwight", "Schrute")],
                    assistant_to_the_regional_managers=[])
 
-        mock.assert_called_with(Person("Dwight", "Schrute"))
-        mock.reset_mock()
+        # In this case, the entire first object is returned as opposed to Pam's
+        # last name
+        regional_manager.assert_called_with(Person("Dwight", "Schrute"))
+        regional_manager.reset_mock()
         token.disconnect()
 
         task = asyncio.get_event_loop().create_task(self.__myfunc(app))
@@ -74,8 +89,9 @@ class TestREADME(unittest.TestCase):
         asyncio.get_event_loop().run_until_complete(task)
 
         self.assertEqual(self.__regional_managers, [Person("Jim", "Halpert")])
-        # we disconnected the token, so mock shouldn't get called again
-        mock.assert_not_called()
+        # we disconnected the token, so no more notifications for the regional
+        # manager
+        regional_manager.assert_not_called()
 
     async def __myfunc(self, app: AppModel) -> None:
         regional_managers = await app.event(
