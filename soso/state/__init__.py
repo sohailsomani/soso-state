145e2b90f86eaf717fb9611edcf8ddfd29721c4f
Sohail Somani <me@sohailsomani.com>
2021-02-10 21:51:10 -0500

    flake

--- soso/state/__init__.py
+++ soso/state/__init__.py
@@ -1,2 +1,2 @@
-from soso.state.event import *  # noqa: F401
-from soso.state.state import *  # noqa: F401
+from soso.state.event import *  # noqa: F401, F403
+from soso.state.state import *  # noqa: F401, F403
