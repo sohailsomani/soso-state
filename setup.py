7d5dc7dec1219a4894377332fc9e1f56c143148a
Sohail Somani <sohail@fizzbuzzinc.com>
2021-03-11 08:17:16 -0500

    Add ability build with mypyc (fails due to use of protocols)

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 # flake8: noqa
 # type: ignore
 
+import glob
+import sys
+
 import setuptools
+from mypyc.build import mypycify
+from setuptools import Extension
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+
+build_with_mypyc = "--with-mypyc" in sys.argv
+if build_with_mypyc:
+    sys.argv.remove("--with-mypyc")
+
+if build_with_mypyc:
+    files = glob.glob("soso/**/*.py",recursive=True)
+    files = [f for f in files if f.find("__init__.py") == -1]
+    ext_modules = mypycify(files)
+else:
+    ext_modules = []
 
 setuptools.setup(
     name="soso-state",
@@ -15,15 +31,15 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sohailsomani/soso-state",
-    packages=['soso','soso.state'],
+    packages=['soso', 'soso.state'],
     namespace_packages=['soso'],
     package_data={
-        'soso':['*.pyi','py.typed'],
-        'soso.state':['*.pyi','py.typed']
+        'soso': ['*.pyi', 'py.typed'],
+        'soso.state': ['*.pyi', 'py.typed']
     },
+    ext_modules = ext_modules,
     classifiers=[
-        "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8", "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
     python_requires=">=3.8",
